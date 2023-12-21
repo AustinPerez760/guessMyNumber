bf852487d92f65eed5251f7d0dfba7677fbeda2d
Austin Perez <austin.perez760@gmail.com>
2023-12-20 22:17:29 -0600

    Used DOM to change text content, created funciton and methods to handle on clicks

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-"use strict";
+'use strict';
+// Manipulating text content for one of theDOM nodes
+document.querySelector('.message').textContent = 'Correct Number';
 
-console.log(document.querySelector(".message").textContent);
+//handling on-click for the check button
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
