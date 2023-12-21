3f7a9fdc03b92873c4988451e72d71a0a6f706d6
Austin Perez <austin.perez760@gmail.com>
2023-12-20 22:30:24 -0600

    converted boolean from false to true using not operator on  'guess' variable

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ document.querySelector('.message').textContent = 'Correct Number';
 
 //handling on-click for the check button
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  //converting string to a number and saving as variable
+  const guess = Number(document.querySelector('.guess').value);
+
+  //converting boolean from false to true
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
 });
