e4eb1c4e72b2c3da3a6bcbc6aa44960f295ea05c
Austin Perez <austin.perez760@gmail.com>
2023-12-23 13:01:03 -0600

    Fixed messge class to have bigger margin between score messages, added random number logic to test user input against.

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 // Manipulating text content for one of theDOM nodes
 document.querySelector('.message').textContent = 'Correct Number';
 
-//handling on-click for the check button
+// Creating a rondom number between 1 and 20, Trunc used to reomce deccimals after whole number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+//displaying number behing 'Question Mark'
+document.querySelector('.number').textContent = secretNumber;
+//handling on-click for the check buttonß
 document.querySelector('.check').addEventListener('click', function () {
   //converting string to a number and saving as variable
   const guess = Number(document.querySelector('.guess').value);
 
-  //converting boolean from false to true
+  //converting boolean from false to true since guess is = 0
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
   }
 });
