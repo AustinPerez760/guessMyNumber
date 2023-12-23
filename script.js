bd59d78bd0f6e7a7c0d0dc0fea42cedeebce4836
Austin Perez <austin.perez760@gmail.com>
2023-12-23 13:50:43 -0600

    Created logic that ends game with a 'Game Over' message if user score reaches 0. score will decreasew by 1 for every wrong guess

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 // Creating a rondom number between 1 and 20, Trunc used to reomce deccimals after whole number
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 
+let score = 5;
+
 //displaying number behing 'Question Mark'
 document.querySelector('.number').textContent = secretNumber;
 //handling on-click for the check buttonß
@@ -12,11 +14,28 @@ document.querySelector('.check').addEventListener('click', function () {
 
   //converting boolean from false to true since guess is = 0
   if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
+    document.querySelector('.message').textContent = '🚫 No number!';
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('.message').textContent = '🎊 Correct Number';
+    score++;
+    document.querySelector('.score').textContent = score;
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High!';
-  } else if (guess < secretNumber)
-    document.querySelector('.message').textContent = 'Too Low!';
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'GAME OVER ❌';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'GAME OVER ❌';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
